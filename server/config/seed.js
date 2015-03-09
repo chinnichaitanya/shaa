a90094f0fefd02b3f2c687b3d99a817628d3d39d
chinnichaitanya <chchaitanya95@gmail.com>
2015-03-09 16:06:42 +0530

    updates

--- server/config/seed.js
+++ server/config/seed.js
@@ -39,9 +39,9 @@ User.find({}).remove(function() {
   }, {
     provider: 'local',
     role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
+    name: 'chinni',
+    email: 'chinni@chinni.com',
+    password: 'chinni'
   }, function() {
       console.log('finished populating users');
     }

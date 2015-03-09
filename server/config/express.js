9afb9e78be8477af2945fd467b3e4cc3fd8ac937
chinnichaitanya <chchaitanya95@gmail.com>
2015-03-09 21:17:41 +0530

    updates

--- server/config/express.js
+++ server/config/express.js
@@ -49,6 +49,8 @@ module.exports = function(app) {
   }
 
   if ('development' === env || 'test' === env) {
+    console.log("root: " + config.root);
+    console.log("yoo: " + path.join(config.root, 'public', 'favicon.ico'));
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
